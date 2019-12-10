fcdeb306a2a50fa6f57ecbe891c67ae862baf061
Jonathan Poh <jpoh@srcclr.com>
2019-12-10 10:31:52 -0800

    WIP fix broken layout (SUI syntax changes)

--- src/App.js
+++ src/App.js
@@ -1,106 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import { Button, Divider, Grid, Header, Icon, Segment, Image, Statistic, Embed, Container } from 'fluorine-ui';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalBarSeries, LineSeries} from 'react-vis';
+import { Grid, Header, Icon, Statistic, Embed } from "semantic-ui-react";
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  VerticalBarSeries,
+  LineSeries
+} from "react-vis";
 
 function App() {
   return (
     <div className="App">
-        <Header color="white" as="h1" size="huge" inverted className="dashboard-header">
-            SourceClear
-        </Header>
+      <Header
+        color="white"
+        as="h1"
+        size="huge"
+        inverted
+        className="dashboard-header"
+      >
+        SourceClear
+      </Header>
 
-        <Grid container columns='equal' relaxed='very' stackable celled  >
-            <Grid.Row stretched size="300px"  >
-                <Grid.Column stretched align="center">
-                        <Icon name="download" color='orange' size='massive' inverted />
-                    <Statistic color="orange" inverted>
-                        <Statistic.Value>5,550</Statistic.Value>
-                        <Statistic.Label>Downloads</Statistic.Label>
-                    </Statistic>
-                </Grid.Column>
+      <Grid
+        container
+        columns="equal"
+        relaxed="very"
+        textAlign="center"
+        stackable
+        celled
+      >
+        <Grid.Row stretched size="300px">
+          <Grid.Column stretched textAlign="center">
+            <Icon
+              name="download"
+              color="orange"
+              size="massive"
+              fitted
+              inverted
+            />
+            <Statistic color="orange" inverted>
+              <Statistic.Value>5,550</Statistic.Value>
+              <Statistic.Label>Downloads</Statistic.Label>
+            </Statistic>
+          </Grid.Column>
 
-                <Grid.Column stretched align="center">
-                    <Icon name="dollar" color='violet' size='massive' inverted />
-                    <Statistic color="violet" inverted>
-                        <Statistic.Value>213</Statistic.Value>
-                        <Statistic.Label>Enterprise Accounts</Statistic.Label>
-                    </Statistic>
-                </Grid.Column>
-                <Grid.Column stretched align="center">
-                    <Icon name="exclamation" color='red' size='massive' inverted />
-                    <Statistic color="red" inverted>
-                        <Statistic.Value>562</Statistic.Value>
-                        <Statistic.Label>Errors</Statistic.Label>
-                    </Statistic>
-                </Grid.Column>
-                <Grid.Column stretched align="center">
-                    <Icon name="code" color='teal' size='massive' inverted />
+          <Grid.Column stretched textAlign="center">
+            <Icon name="dollar" color="violet" size="massive" fitted inverted />
+            <Statistic color="violet" inverted>
+              <Statistic.Value>213</Statistic.Value>
+              <Statistic.Label>Enterprise Accounts</Statistic.Label>
+            </Statistic>
+          </Grid.Column>
+          <Grid.Column stretched textAlign="center">
+            <Icon
+              name="exclamation"
+              color="red"
+              size="massive"
+              fitted
+              inverted
+            />
+            <Statistic color="red" inverted>
+              <Statistic.Value>562</Statistic.Value>
+              <Statistic.Label>Errors</Statistic.Label>
+            </Statistic>
+          </Grid.Column>
+          <Grid.Column stretched textAlign="center">
+            <Icon name="code" color="teal" size="massive" fitted inverted />
 
-                    <Statistic color="teal" inverted>
-                        <Statistic.Value>12</Statistic.Value>
-                        <Statistic.Label>Open Pull Requests</Statistic.Label>
-                    </Statistic>
-                </Grid.Column>
-            </Grid.Row>
+            <Statistic color="teal" inverted>
+              <Statistic.Value>12</Statistic.Value>
+              <Statistic.Label>Open Pull Requests</Statistic.Label>
+            </Statistic>
+          </Grid.Column>
+        </Grid.Row>
 
-            <Grid.Row stretched>
-                <Grid.Column>
-                    <Embed id='yWhD0dhMd9U' placeholder='http://placekitten.com/300/300' source='youtube' autoplay={true} />
-                </Grid.Column>
-                <Grid.Column>
-                    <XYPlot
-                        width={300}
-                        height={300}>
-                        <HorizontalGridLines />
-                        <LineSeries
-                            data={[
-                                {x: 1, y: 10},
-                                {x: 2, y: 4},
-                                {x: 3, y: 0},
-                                {x: 4, y: 12},
-                                {x: 5, y: 5},
-                                {x: 6, y: 8},
-                                {x: 7, y: 15}
-                            ]}/>
-                        <XAxis />
-                        <YAxis />
-                    </XYPlot>
-                </Grid.Column>
-                <Grid.Column>
-                    <XYPlot
-                        width={300}
-                        height={300}>
-                        <HorizontalGridLines />
-                        <VerticalBarSeries
-                            data={[
-                                {x: 1, y: 10},
-                                {x: 2, y: 5},
-                                {x: 3, y: 15}
-                            ]}/>
-                        <XAxis />
-                        <YAxis />
-                    </XYPlot>
-                </Grid.Column>
-                <Grid.Column>
-                    <XYPlot
-                        width={300}
-                        height={300}>
-                        <HorizontalGridLines />
-                        <LineSeries
-                            data={[
-                                {x: 1, y: 10},
-                                {x: 2, y: 5},
-                                {x: 3, y: 15}
-                            ]}/>
-                        <XAxis />
-                        <YAxis />
-                    </XYPlot>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
+        <Grid.Row stretched>
+          <Grid.Column>
+            <Embed
+              id="yWhD0dhMd9U"
+              placeholder="http://placekitten.com/300/300"
+              source="youtube"
+              autoplay={true}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <XYPlot width={250} height={250}>
+              <HorizontalGridLines />
+              <LineSeries
+                data={[
+                  { x: 1, y: 10 },
+                  { x: 2, y: 4 },
+                  { x: 3, y: 0 },
+                  { x: 4, y: 12 },
+                  { x: 5, y: 5 },
+                  { x: 6, y: 8 },
+                  { x: 7, y: 15 }
+                ]}
+              />
+              <XAxis />
+              <YAxis />
+            </XYPlot>
+          </Grid.Column>
+          <Grid.Column>
+            <XYPlot width={250} height={250}>
+              <HorizontalGridLines />
+              <VerticalBarSeries
+                data={[
+                  { x: 1, y: 10 },
+                  { x: 2, y: 5 },
+                  { x: 3, y: 15 }
+                ]}
+              />
+              <XAxis />
+              <YAxis />
+            </XYPlot>
+          </Grid.Column>
+          <Grid.Column>
+            <XYPlot width={250} height={250}>
+              <HorizontalGridLines />
+              <LineSeries
+                data={[
+                  { x: 1, y: 10 },
+                  { x: 2, y: 5 },
+                  { x: 3, y: 15 }
+                ]}
+              />
+              <XAxis />
+              <YAxis />
+            </XYPlot>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'fluorine-ui/dist/fluorine-ui.css';
-import 'react-vis/dist/style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import "react-vis/dist/style.css";
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
